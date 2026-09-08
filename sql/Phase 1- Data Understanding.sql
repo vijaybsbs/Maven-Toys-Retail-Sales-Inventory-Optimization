18f1273dec8a36dccbeacdb8a70cc7139b395f39
Vijay <vijaybsbs@gmail.com>
2026-09-08 06:10:00 +0530

    Update Phase 1- Data Understanding.sql

--- sql/Phase 1- Data Understanding.sql
+++ sql/Phase 1- Data Understanding.sql
@@ -1,8 +1,12 @@
-Phase 1- Data Understanding
+-- ========================================================================================================================
+-- PHASE 1 - DATA UNDERSTANDING
+-- ========================================================================================================================
 
-
+-- ========================================================================================================================
 -- 1. Table Profiling
+-- Objective:  
 -- Find row counts and identify the primary/key columns.
+-- ========================================================================================================================
 
 SELECT 'inventory' AS table_name, COUNT(*) AS row_count
 FROM `maven.inventory`
@@ -16,11 +20,11 @@ UNION ALL
 SELECT 'sales' AS table_name, COUNT(*) AS row_count
 FROM `maven.sales`
 
-
-
+-- ========================================================================================================================
 -- 2. Date Range & Transactions
 -- Find minimum/maximum sales date, number of selling days and transactions.
-
+-- ========================================================================================================================
+  
 select
   min(Date) as min_sales_date,
   max(Date) as max_sales_date,
@@ -28,8 +32,10 @@ select
   count(distinct Sale_ID) as transactions
 from `maven.sales`
 
+-- ========================================================================================================================
 -- 3. Referential Integrity
 -- Check whether all sales have valid stores and products.
+-- ========================================================================================================================
 
 select distinct sale_id, st.Store_ID, p.Product_ID  from `maven.sales` s
 left join `maven.stores` st
@@ -38,9 +44,11 @@ left join `maven.products` p
 on s.Product_ID = p.Product_ID
 where st.Store_ID is null or p.Product_ID is null
 
+-- ========================================================================================================================
 -- 4. Duplicate Detection
 -- Check duplicates in all four tables.
-
+-- ========================================================================================================================
+  
 select
   "stores" as table_name,
   count(*) as total_rows,
@@ -69,11 +77,12 @@ select
   count(*) - count (distinct concat(product_id, product_name, product_category, product_cost, product_price)) as duplicate_rows
 from `maven.products`
 
-
+-- ========================================================================================================================
 -- 5. Inventory Completeness
 -- There are 50 × 35 = 1,750 possible store-product combinations.
 -- The inventory table contains 1,593 records.
 -- Find the missing combinations.
+-- ========================================================================================================================
 
 select
   s.store_id,
@@ -88,8 +97,9 @@ where not exists
   and i.product_id = p.product_id
 )
 
+-- ========================================================================================================================
 -- 5a. Calculate the percentage of possible store-product combinations that are missing.
-
+-- ========================================================================================================================
 
 select
   count(*) as total_combinations,
